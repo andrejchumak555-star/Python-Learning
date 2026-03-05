297ba521e4829b319539cdf0b4f52b4cec99e506
Andrew Chumakov <andrejchumak555@gmail.com>
2026-03-05 20:26:05 +0200

    My first commit

---
+++ Python.py
@@ -0,0 +1,19 @@
+a = float(input("Введи число!"))
+b = float(input("Введи 2 число!"))
+op = input("Введи операцию!")
+def calculate(a,b, op):
+    if op == "+":
+      return a+b
+    elif op == "-":
+      return a-b
+    elif op == "*":
+     return a*b
+    elif op == "/":
+      if b != 0:
+           return a / b
+      else:
+          return "Делить на ноль нельзя!"
+    else:
+      return "Неизвестная операция!"
+result = calculate(a,b, op)
+print("Вот твой резальтат!", result)
